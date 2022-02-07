f5e8bd0fed926b03d53a038d7793e302d37bc698
balbino aylagas <balbino_a@hotmail.com>
2022-02-06 22:54:05 -0600

    update files, created show table

--- app.py
+++ app.py
@@ -7,11 +7,20 @@ app = Flask(__name__)
 def home():
     name = "pedro"
     showParagraph = True
+    dayOfWeek = 100
+    numberList = ["bombom", "bellota", "burbuja"]
     return render_template(
         "home.html",
         name=name,
         showParagraph=showParagraph,
+        dayOfWeek=dayOfWeek,
+        numberList=numberList,
     )
+
+
+@app.route("/table")
+def showTable():
+    return render_template("table.html")
 
 
 if __name__ == "__main__":
